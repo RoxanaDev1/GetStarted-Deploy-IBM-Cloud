f53bd621c2b80b6bf5b4236720b6ab6fb8d9fe7e
RoxanaDev <roxana.balanm@gmail.com>
2019-01-18 18:09:13 +0100

    Update the app and add a cute picture

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import SimpleComponent from "./components/SimpleComponent"
+import SimpleComponent from "./components/SimpleComponent";
 
 export default class App extends React.Component {
-    render() {
-        return <div>
-            <label>This is the App component</label>
-            <SimpleComponent text={"This is a simple component"}/>
-            <SimpleComponent text={"This is another simple component"}/>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <label>This is your app!</label>
+        <SimpleComponent />
+      </div>
+    );
+  }
 }
--- src/client/components/SimpleComponent.tsx
+++ src/client/components/SimpleComponent.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
+import { CSSProperties } from "react";
 
-interface SimpleComponentProps {
-    text: string;
-}
+export default class SimpleComponent extends React.Component<{}, {}> {
+  private simpleComponentStyle: CSSProperties = {
+    height: "100vh"
+  };
 
-export default class SimpleComponent extends React.Component<SimpleComponentProps, {}> {
-
-    render() {
-        return <div>{this.props.text}</div>
-    }
+  render() {
+    return (
+      <div style={this.simpleComponentStyle}>
+        <iframe
+          src="https://giphy.com/embed/bbSrmJPHG8JbaGRZMZ"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
+    );
+  }
 }
